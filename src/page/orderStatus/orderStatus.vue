a5f01a809ccb49d00f4a5f6cb0ff33f76ffba5d5
snakeYu <897910109@qq.com>
2018-05-22 16:08:03 +0800

    added, 初始化项目

---
+++ src/page/orderStatus/orderStatus.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    s
+  </div>
+</template>
+<script>
+export default {
+  name: 'orderStatus'
+}
+</script>
+<style lang="scss" scoped>
+@import '../../scss/var.scss';
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const state = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const getters = {}
+const store = new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
+})
+export default store
